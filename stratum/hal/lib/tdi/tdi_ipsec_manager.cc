17bd53322498d497b7ffe4c29ce0a4eb8700fe46
Sabeel Ansari <sabeel.ansari@intel.com>
2023-01-09 11:58:29 -0800

    Adding IPsec manager files
    Signed-off-by: Sabeel Ansari <sabeel.ansari@intel.com>

---
+++ stratum/hal/lib/tdi/tdi_ipsec_manager.cc
@@ -0,0 +1,177 @@
+// Copyright (c) 2022-2023 Intel Corporation.
+// SPDX-License-Identifier: Apache-2.0
+
+#include "stratum/hal/lib/tdi/tdi_ipsec_manager.h"
+
+#include <map>
+#include <memory>
+#include <set>
+#include <utility>
+
+#include "absl/base/thread_annotations.h"
+#include "absl/memory/memory.h"
+#include "absl/synchronization/mutex.h"
+#include "absl/time/time.h"
+#include "absl/types/optional.h"
+#include "stratum/glue/integral_types.h"
+#include "stratum/hal/lib/common/constants.h"
+#include "stratum/hal/lib/common/utils.h"
+#include "stratum/hal/lib/common/writer_interface.h"
+#include "stratum/lib/channel/channel.h"
+#include "stratum/lib/constants.h"
+#include "stratum/lib/macros.h"
+#include "stratum/lib/utils.h"
+#include "stratum/glue/logging.h"
+
+#define IPSEC_CONFIG_SADB_TABLE_NAME  "ipsec-offload.ipsec-offload.sad.sad-entry.ipsec-sa-config"
+#define IPSEC_FETCH_SPI_TABLE_NAME    "ipsec-offload.ipsec-offload.ipsec-spi"
+#define IPSEC_NOTIFICATION_TABLE_NAME "ipsec-offload"
+#define IPSEC_STATE_SADB_TABLE_NAME   "ipsec-offload.ipsec-offload.sad.sad-entry.ipsec-sa-state"
+
+namespace stratum {
+namespace hal {
+namespace tdi {
+
+ABSL_CONST_INIT absl::Mutex _ipsec_mgr_lock(absl::kConstInit);
+
+/* #### C function callback #### */
+void ipsec_notification_callback(uint32_t dev_id,
+                                 uint32_t ipsec_sa_spi,
+                                 bool soft_lifetime_expire,
+                                 uint8_t ipsec_sa_protocol,
+                                 char *ipsec_sa_dest_address,
+                                 bool ipv4,
+                                 void *cookie) {
+  //printf("IPsec callback: dev_id=%d, ipsec_sa_spi=%d, soft_lifetime=%d, ipsec_sa_protocol=%d, \
+  //       ipsec_sa_dest_address=%s, ipv4=%d, cookie=%p\n",
+  //       dev_id, ipsec_sa_spi, soft_lifetime_expire, ipsec_sa_protocol, ipsec_sa_dest_address, ipv4, cookie);
+  auto ipsec_mgr_hdl = reinterpret_cast<IPsecManager *>(cookie);
+  ipsec_mgr_hdl->SendSADExpireNotificationEvent(dev_id, ipsec_sa_spi, soft_lifetime_expire,
+                                                ipsec_sa_protocol, ipsec_sa_dest_address, ipv4);
+}
+
+IPsecManager::IPsecManager(TdiSdeInterface* tdi_sde_interface, TdiFixedFunctionManager* tdi_fixed_function_manager)
+    : tdi_sde_interface_(ABSL_DIE_IF_NULL(tdi_sde_interface)),
+      tdi_fixed_function_manager_(ABSL_DIE_IF_NULL(tdi_fixed_function_manager)),
+      notif_initialized_(false) {}
+
+IPsecManager::IPsecManager()
+    : tdi_sde_interface_(nullptr),
+      tdi_fixed_function_manager_(nullptr),
+      gnmi_event_writer_(nullptr) {}
+
+IPsecManager::~IPsecManager() = default;
+
+::util::Status IPsecManager::InitializeNotificationCallback() {
+  auto status = tdi_fixed_function_manager_->InitNotificationTableWithCallback(IPSEC_NOTIFICATION_TABLE_NAME,
+                                                                               &ipsec_notification_callback,
+                                                                               this);
+  if (status != ::util::OkStatus()) {
+    LOG(ERROR) << "Failed to register IPsec notification callback";
+  }
+  return ::util::OkStatus();
+}
+
+::util::Status IPsecManager::GetSpiData(uint32 &fetched_spi) {
+  // TODO (5abeel): Initilizing the notification callback on FetchSPI because TDI layer
+  // is not initialized until 'set-pipe' is completed by user via P4RT
+  if (!notif_initialized_) {
+      auto status = InitializeNotificationCallback();
+      if (status == ::util::OkStatus()) {
+        notif_initialized_ = true;
+      }
+  }
+  
+  ASSIGN_OR_RETURN(auto session, tdi_sde_interface_->CreateSession());
+  auto status = tdi_fixed_function_manager_->FetchSpi(session, 
+                                                      IPSEC_FETCH_SPI_TABLE_NAME,
+                                                      &fetched_spi);
+  if (status != ::util::OkStatus()) {
+    return MAKE_ERROR(ERR_AT_LEAST_ONE_OPER_FAILED)
+           << "One or more read operations failed.";
+  }
+  LOG(INFO) << "FetchSPI read operation successful with SPI value = "
+            << fetched_spi;
+  return ::util::OkStatus();
+}
+
+::util::Status IPsecManager::SetConfigSADEntry(IPsecSADConfig *msg) {
+  // TODO (5abeel): Initilizing the notification callback on FetchSPI because TDI layer
+  // is not initialized until 'set-pipe' is completed by user via P4RT
+  if (!notif_initialized_) {
+      auto status = InitializeNotificationCallback();
+      if (status == ::util::OkStatus()) {
+        notif_initialized_ = true;
+      }
+  }
+
+  return writeConfigSADEntry(IPSEC_CONFIG_SADB_TABLE_NAME,
+                             IPsecSadOp::IPSEC_SADB_CONFIG_OP_ADD_ENTRY,
+                             msg);
+}
+
+::util::Status IPsecManager::DeleteConfigSADEntry(uint32 &offload_id, bool &direction) {
+  // Delete op in TdiFixedFunctionManager::WriteSadbEntry() only accesses the offload_id
+  // and direction, so create a IPsecSADConfig with the offload_id and direction set and
+  // call writeConfigSADEntry
+  IPsecSADConfig msg;
+  msg.set_offload_id(offload_id);
+  msg.set_direction(direction);
+
+  return writeConfigSADEntry(IPSEC_CONFIG_SADB_TABLE_NAME,
+                             IPsecSadOp::IPSEC_SADB_CONFIG_OP_DEL_ENTRY,
+                             &msg);
+}
+
+::util::Status IPsecManager::writeConfigSADEntry(std::string table_name,
+                                                 enum IPsecSadOp op_type,
+                                                 IPsecSADConfig *msg) {
+  ASSIGN_OR_RETURN(auto session, tdi_sde_interface_->CreateSession());
+  auto status = tdi_fixed_function_manager_->WriteSadbEntry(session, table_name, op_type, msg);
+  if (status != ::util::OkStatus()) {
+    return MAKE_ERROR(ERR_AT_LEAST_ONE_OPER_FAILED)
+           << "One or more write operations failed offload-id=" << msg->offload_id()
+           << ", direction=" << msg->direction()
+           << ", op type=" << op_type
+           << ", table_name=" << table_name;
+  }
+  LOG(INFO) << "ConfigSA write operation completed successfully for "
+            << "offload-id=" << msg->offload_id()
+            << ", direction=" << msg->direction()
+            << ", op type=" << op_type
+            << ", table_name=" << table_name;
+  return ::util::OkStatus();
+
+}
+
+void IPsecManager::SendSADExpireNotificationEvent(uint32_t dev_id,
+                                                  uint32_t ipsec_sa_spi,
+                                                  bool soft_lifetime_expire,
+                                                  uint8_t ipsec_sa_protocol,
+                                                  char *ipsec_sa_dest_address,
+                                                  bool ipv4) {
+  absl::ReaderMutexLock l(&gnmi_event_lock_);
+  if (!gnmi_event_writer_) return;
+  // Allocate and initialize a IPsecNotificationEvent event and pass it to
+  // the gNMI publisher using the gNMI event notification channel.
+  // The GnmiEventPtr is a smart pointer (shared_ptr<>) and it takes care of
+  // the memory allocated to this event object once the event is handled by
+  // the GnmiPublisher.
+  if (!gnmi_event_writer_->Write(GnmiEventPtr(new IPsecNotificationEvent(
+          absl::ToUnixNanos(absl::UnixEpoch()),
+          dev_id, ipsec_sa_spi, soft_lifetime_expire,
+          ipsec_sa_protocol, ipsec_sa_dest_address, ipv4)))) {
+    // Remove WriterInterface if it is no longer operational.
+    gnmi_event_writer_.reset();
+  }
+}
+
+std::unique_ptr<IPsecManager> IPsecManager::CreateInstance(
+    TdiSdeInterface* tdi_sde_interface, TdiFixedFunctionManager* tdi_fixed_function_manager) {
+  return absl::WrapUnique(
+      new IPsecManager(tdi_sde_interface, tdi_fixed_function_manager));
+}
+
+}  // namespace tdi
+}  // namespace hal
+}  // namespace stratum
---
+++ stratum/hal/lib/tdi/tdi_ipsec_manager.h
@@ -0,0 +1,128 @@
+// Copyright (c) 2022-2023 Intel Corporation.
+// SPDX-License-Identifier: Apache-2.0
+
+#ifndef STRATUM_HAL_LIB_TDI_IPSEC_MANAGER_H_
+#define STRATUM_HAL_LIB_TDI_IPSEC_MANAGER_H_
+
+#include <map>
+#include <memory>
+
+#include "absl/base/thread_annotations.h"
+#include "absl/memory/memory.h"
+#include "absl/synchronization/mutex.h"
+#include "absl/time/time.h"
+#include "absl/types/optional.h"
+#include "stratum/glue/integral_types.h"
+#include "stratum/hal/lib/common/gnmi_events.h"
+#include "stratum/hal/lib/common/utils.h"
+#include "stratum/hal/lib/common/writer_interface.h"
+#include "stratum/hal/lib/tdi/tdi_fixed_function_manager.h"
+#include "stratum/hal/lib/tdi/tdi_sde_interface.h"
+#include "stratum/lib/channel/channel.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void ipsec_notification_callback(uint32_t dev_id,
+                                 uint32_t ipsec_sa_spi,
+                                 bool soft_lifetime_expire,
+                                 uint8_t ipsec_sa_protocol,
+                                 char *ipsec_sa_dest_address,
+                                 bool ipv4,
+                                 void *cookie);
+
+#ifdef __cplusplus
+}  // extern "C"
+#endif
+
+namespace stratum {
+namespace hal {
+namespace tdi {
+
+// Lock which protects IPsecMgr state across the entire switch.
+extern absl::Mutex _ipsec_mgr_lock;
+
+class IPsecManager {
+ public:
+  virtual ~IPsecManager();
+
+  // Factory function for creating the instance of the class.
+  static std::unique_ptr<IPsecManager> CreateInstance(
+      TdiSdeInterface* tdi_sde_interface,
+      TdiFixedFunctionManager* tdi_fixed_function_manager);
+
+  virtual ::util::Status InitializeNotificationCallback();
+
+  virtual ::util::Status GetSpiData(uint32 &fetched_spi)
+      SHARED_LOCKS_REQUIRED(_ipsec_mgr_lock);
+
+  virtual ::util::Status SetConfigSADEntry(IPsecSADConfig *msg)
+      SHARED_LOCKS_REQUIRED(_ipsec_mgr_lock);
+
+  virtual ::util::Status DeleteConfigSADEntry(uint32 &offload_id, bool &direction)
+      SHARED_LOCKS_REQUIRED(_ipsec_mgr_lock);
+
+  // IPsecManager is neither copyable nor movable.
+  IPsecManager(const IPsecManager&) = delete;
+  IPsecManager& operator=(const IPsecManager&) = delete;
+  IPsecManager(IPsecManager&&) = delete;
+  IPsecManager& operator=(IPsecManager&&) = delete;
+
+  virtual ::util::Status RegisterEventNotifyWriter(
+      const std::shared_ptr<WriterInterface<GnmiEventPtr>>& writer) {
+          absl::WriterMutexLock l(&gnmi_event_lock_);
+          gnmi_event_writer_ = writer;
+          return ::util::OkStatus();
+  }
+
+  virtual ::util::Status UnregisterEventNotifyWriter() {
+    absl::WriterMutexLock l(&gnmi_event_lock_);
+    gnmi_event_writer_ = nullptr;
+    return ::util::OkStatus();
+  }
+
+  void SendSADExpireNotificationEvent(uint32_t dev_id,
+                                      uint32_t ipsec_sa_spi,
+                                      bool soft_lifetime_expire,
+                                      uint8_t ipsec_sa_protocol,
+                                      char *ipsec_sa_dest_address,
+                                      bool ipv4);
+
+ protected:
+  // Default constructor. To be called by the Mock class instance only.
+  IPsecManager();
+
+ private:
+  // ReaderArgs encapsulates the arguments for a Channel reader thread.
+  template <typename T>
+  struct ReaderArgs {
+    IPsecManager* manager;
+    std::unique_ptr<ChannelReader<T>> reader;
+  };
+
+  // Private constructor. Use CreateInstance() to create an instance of this
+  // class.
+  IPsecManager(TdiSdeInterface* tdi_sde_interface, TdiFixedFunctionManager* tdi_fixed_function_manager);
+
+  virtual ::util::Status writeConfigSADEntry(std::string table_name, enum IPsecSadOp op_type, IPsecSADConfig *msg)
+      SHARED_LOCKS_REQUIRED(_ipsec_mgr_lock);
+
+  // WriterInterface<GnmiEventPtr> object for sending event notifications.
+  mutable absl::Mutex gnmi_event_lock_;
+  std::shared_ptr<WriterInterface<GnmiEventPtr>> gnmi_event_writer_
+      GUARDED_BY(gnmi_event_lock_);
+
+  // Pointer to TdiSdeInterface implementation and FixedFunctionManager
+  TdiSdeInterface* tdi_sde_interface_;  // not owned by this class.
+  TdiFixedFunctionManager* tdi_fixed_function_manager_;  // not owned by this class.
+  bool notif_initialized_;
+
+  friend class IPsecManagerTest;
+};
+
+}  // namespace tdi
+}  // namespace hal
+}  // namespace stratum
+
+#endif  // STRATUM_HAL_LIB_TDI_IPSEC_MANAGER_H_
