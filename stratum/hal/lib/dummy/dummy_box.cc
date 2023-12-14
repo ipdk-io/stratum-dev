427c5682da9475a4bf41fd53070d541fa1e6c076
Derek G Foster <derek.foster@intel.com>
2023-12-14 10:23:01 -0800

    Remove invalid response allocation in DummyNode gRPC call
    Signed-off-by: Derek G Foster <derek.foster@intel.com>

--- stratum/hal/lib/dummy/dummy_box.cc
+++ stratum/hal/lib/dummy/dummy_box.cc
@@ -217,7 +217,7 @@ DummyBox* DummyBox::GetSingleton() {
 
 DummyBox::~DummyBox() {}
 DummyBox::DummyBox()
-    : initialized_(false), xcvr_writer_id_(0), external_server_tid_() {}
+    : initialized_(false), xcvr_writer_id_(0), external_server_tid_(0) {}
 
 }  // namespace dummy_switch
 }  // namespace hal
