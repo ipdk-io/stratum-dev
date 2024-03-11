96decfa0ef0df0af237078a58ece8c8690b3d2e0
Kumar, Aashish <aashish.kumar@intel.com>
2024-03-10 21:49:25 -0400

    mock .h file update
    Signed-off-by: Kumar, Aashish <aashish.kumar@intel.com>

--- stratum/hal/lib/p4/p4_info_manager_mock.h
+++ stratum/hal/lib/p4/p4_info_manager_mock.h
@@ -70,20 +70,20 @@ class P4InfoManagerMock : public P4InfoManager {
 
   // FindPktModMeter
   MOCK_CONST_METHOD1(FindPktModMeterByID,
-                     ::util::StatusOr<const ::p4::config::v1::PacketModMeter>(
+                     ::util::StatusOr<const ::idpf::PacketModMeter>(
                          uint32 meter_id));
   MOCK_CONST_METHOD1(FindPktModMeterByName,
-                     ::util::StatusOr<const ::p4::config::v1::PacketModMeter>(
+                     ::util::StatusOr<const ::idpf::PacketModMeter>(
                          const std::string& meter_name));
 
   // FindDirectPktModMeter
   MOCK_CONST_METHOD1(
       FindDirectPktModMeterByID,
-      ::util::StatusOr<const ::p4::config::v1::DirectPacketModMeter>(
+      ::util::StatusOr<const ::idpf::DirectPacketModMeter>(
           uint32 meter_id));
   MOCK_CONST_METHOD1(
       FindDirectPktModMeterByName,
-      ::util::StatusOr<const ::p4::config::v1::DirectPacketModMeter>(
+      ::util::StatusOr<const ::idpf::DirectPacketModMeter>(
           const std::string& meter_name));
 
   // FindValueSet
