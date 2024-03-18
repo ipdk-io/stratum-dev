589e4022b3e29c84183fb663402097d967d39d00
Derek G Foster <derek.foster@intel.com>
2024-03-18 13:02:37 -0700

    Revamp tdi_bf_status.h to be target-neutral
    - Revised tdi_bf_status.h to represent a TDI status code, rather
      than a legacy BF status code or an ES2K IPU status code. This is
      how the class should have been implemented in the first place,
      since it is intended to operate on status codes returned by TDI.
    
      This particular implementation relies on the 1:1:1 correspondence
      between BF, TDI, and IPU status codes. This is not as outrageous
      as it may at first seem: the Stratum TDI code currently uses
      RETURN_IF_TDI_ERROR() indiscriminately for both TDI methods and
      bf_pal function calls.
    
      Should this ever change, the target-specific unit tests will
      catch the problem early, and we can deal with the issue then.
    
      I plan to rename tdi_bf_status.h to tdi_status.h in a subsequent
      commit.
    
    Signed-off-by: Derek G Foster <derek.foster@intel.com>

--- stratum/hal/lib/tdi/tdi_bf_status.h
+++ stratum/hal/lib/tdi/tdi_bf_status.h
@@ -1,16 +1,12 @@
 // Copyright 2020-present Open Networking Foundation
-// Copyright 2022-2023 Intel Corporation
+// Copyright 2022-2024 Intel Corporation
 // SPDX-License-Identifier: Apache-2.0
 
 #ifndef STRATUM_HAL_LIB_TDI_TDI_BF_STATUS_H_
 #define STRATUM_HAL_LIB_TDI_TDI_BF_STATUS_H_
 
 extern "C" {
-#ifdef ES2K_TARGET
-#include "ipu_types/ipu_types.h"
-#else
-#include "bf_types/bf_types.h"
-#endif
+#include "tdi/common/tdi_defs.h"
 }
 
 #include "stratum/glue/status/status.h"
@@ -21,139 +17,76 @@ namespace stratum {
 namespace hal {
 namespace tdi {
 
-// Wrapper object for a ipu_status code from the SDE.
-class TdiBfStatus {
+// Wrapper object for a tdi_status code from the SDE.
+class TdiStatus {
  public:
-#ifdef ES2K_TARGET
-  explicit TdiBfStatus(ipu_status_t status) : status_(status) {}
-  operator bool() const { return status_ == IPU_SUCCESS; }
-  inline ipu_status_t status() const { return status_; }
+  explicit TdiStatus(tdi_status_t status) : status_(status) {}
+  operator bool() const { return status_ == TDI_SUCCESS; }
+  inline tdi_status_t status() const { return status_; }
   inline ErrorCode error_code() const {
     switch (status_) {
-      case IPU_SUCCESS:
+      case TDI_SUCCESS:
         return ERR_SUCCESS;
-      case IPU_NOT_READY:
+      case TDI_NOT_READY:
         return ERR_NOT_INITIALIZED;
-      case IPU_INVALID_ARG:
+      case TDI_INVALID_ARG:
         return ERR_INVALID_PARAM;
-      case IPU_ALREADY_EXISTS:
+      case TDI_ALREADY_EXISTS:
         return ERR_ENTRY_EXISTS;
-      case IPU_NO_SYS_RESOURCES:
-      case IPU_MAX_SESSIONS_EXCEEDED:
-      case IPU_NO_SPACE:
-      case IPU_EAGAIN:
+      case TDI_NO_SYS_RESOURCES:
+      case TDI_MAX_SESSIONS_EXCEEDED:
+      case TDI_NO_SPACE:
+      case TDI_EAGAIN:
         return ERR_NO_RESOURCE;
-      case IPU_ENTRY_REFERENCES_EXIST:
+      case TDI_ENTRY_REFERENCES_EXIST:
         return ERR_FAILED_PRECONDITION;
-      case IPU_TXN_NOT_SUPPORTED:
-      case IPU_NOT_SUPPORTED:
+      case TDI_TXN_NOT_SUPPORTED:
+      case TDI_NOT_SUPPORTED:
         return ERR_OPER_NOT_SUPPORTED;
-      case IPU_HW_COMM_FAIL:
-      case IPU_HW_UPDATE_FAILED:
+      case TDI_HW_COMM_FAIL:
+      case TDI_HW_UPDATE_FAILED:
         return ERR_HARDWARE_ERROR;
-      case IPU_NO_LEARN_CLIENTS:
+      case TDI_NO_LEARN_CLIENTS:
         return ERR_FEATURE_UNAVAILABLE;
-      case IPU_IDLE_UPDATE_IN_PROGRESS:
+      case TDI_IDLE_UPDATE_IN_PROGRESS:
         return ERR_OPER_STILL_RUNNING;
-      case IPU_OBJECT_NOT_FOUND:
-      case IPU_TABLE_NOT_FOUND:
+      case TDI_OBJECT_NOT_FOUND:
+      case TDI_TABLE_NOT_FOUND:
         return ERR_ENTRY_NOT_FOUND;
-      case IPU_NOT_IMPLEMENTED:
+      case TDI_NOT_IMPLEMENTED:
         return ERR_UNIMPLEMENTED;
-      case IPU_SESSION_NOT_FOUND:
-      case IPU_INIT_ERROR:
-      case IPU_TABLE_LOCKED:
-      case IPU_IO:
-      case IPU_UNEXPECTED:
-      case IPU_DEVICE_LOCKED:
-      case IPU_INTERNAL_ERROR:
-      case IPU_IN_USE:
+      case TDI_SESSION_NOT_FOUND:
+      case TDI_INIT_ERROR:
+      case TDI_TABLE_LOCKED:
+      case TDI_IO:
+      case TDI_UNEXPECTED:
+      case TDI_DEVICE_LOCKED:
+      case TDI_INTERNAL_ERROR:
+      case TDI_IN_USE:
       default:
         return ERR_INTERNAL;
     }
   }
-#else
-  explicit TdiBfStatus(bf_status_t status) : status_(status) {}
-  operator bool() const { return status_ == BF_SUCCESS; }
-  inline bf_status_t status() const { return status_; }
-  inline ErrorCode error_code() const {
-    switch (status_) {
-      case BF_SUCCESS:
-        return ERR_SUCCESS;
-      case BF_NOT_READY:
-        return ERR_NOT_INITIALIZED;
-      case BF_INVALID_ARG:
-        return ERR_INVALID_PARAM;
-      case BF_ALREADY_EXISTS:
-        return ERR_ENTRY_EXISTS;
-      case BF_NO_SYS_RESOURCES:
-      case BF_MAX_SESSIONS_EXCEEDED:
-      case BF_NO_SPACE:
-      case BF_EAGAIN:
-        return ERR_NO_RESOURCE;
-      case BF_ENTRY_REFERENCES_EXIST:
-        return ERR_FAILED_PRECONDITION;
-      case BF_TXN_NOT_SUPPORTED:
-      case BF_NOT_SUPPORTED:
-        return ERR_OPER_NOT_SUPPORTED;
-      case BF_HW_COMM_FAIL:
-      case BF_HW_UPDATE_FAILED:
-        return ERR_HARDWARE_ERROR;
-      case BF_NO_LEARN_CLIENTS:
-        return ERR_FEATURE_UNAVAILABLE;
-      case BF_IDLE_UPDATE_IN_PROGRESS:
-        return ERR_OPER_STILL_RUNNING;
-      case BF_OBJECT_NOT_FOUND:
-      case BF_TABLE_NOT_FOUND:
-        return ERR_ENTRY_NOT_FOUND;
-      case BF_NOT_IMPLEMENTED:
-        return ERR_UNIMPLEMENTED;
-      case BF_SESSION_NOT_FOUND:
-      case BF_INIT_ERROR:
-      case BF_TABLE_LOCKED:
-      case BF_IO:
-      case BF_UNEXPECTED:
-      case BF_DEVICE_LOCKED:
-      case BF_INTERNAL_ERROR:
-      case BF_IN_USE:
-      default:
-        return ERR_INTERNAL;
-    }
-  }
-#endif
 
  private:
-#ifdef ES2K_TARGET
-  ipu_status_t status_;
-#else
-  bf_status_t status_;
-#endif
+  tdi_status_t status_;
 };
 
-// A macro to simplify checking and logging the return value of a SDE function
-// call.
-#ifdef ES2K_TARGET
+// A macro to simplify checking and logging the return value of an SDE
+// function call.
 #define RETURN_IF_TDI_ERROR(expr)                             \
-  if (const TdiBfStatus __ret = TdiBfStatus(expr)) {          \
+  if (const TdiStatus __ret = TdiStatus(expr)) {              \
   } else /* NOLINT */                                         \
     return MAKE_ERROR(__ret.error_code())                     \
            << "'" << #expr << "' failed with error message: " \
-           << FixMessage(ipu_err_str(__ret.status()))
-#else
-#define RETURN_IF_TDI_ERROR(expr)                             \
-  if (const TdiBfStatus __ret = TdiBfStatus(expr)) {          \
-  } else /* NOLINT */                                         \
-    return MAKE_ERROR(__ret.error_code())                     \
-           << "'" << #expr << "' failed with error message: " \
-           << FixMessage(bf_err_str(__ret.status()))
-#endif
+           << FixMessage(tdi_err_str(__ret.status()))
+
 // A macro to simplify creating a new error or appending new info to an
-// error based on the return value of a SDE function call. The caller function
-// will not return. The variable given as "status" must be an object of type
-// ::util::Status.
-#ifdef ES2K_TARGET
-#define APPEND_STATUS_IF_BFRT_ERROR(status, expr)                           \
-  if (const TdiBfStatus __ret = TdiBfStatus(expr)) {                        \
+// error based on the return value of an SDE function call. The function that
+// invokes the macro does not return to the calling function. The "status"
+// parameter must be an object of type ::util::Status.
+#define APPEND_STATUS_IF_TDI_ERROR(status, expr)                            \
+  if (const TdiStatus __ret = TdiStatus(expr)) {                            \
   } else /* NOLINT */                                                       \
     status =                                                                \
         APPEND_ERROR(!status.ok() ? status                                  \
@@ -165,23 +98,8 @@ class TdiBfStatus {
                 ? ""                                                        \
                 : " ")                                                      \
         << "'" << #expr << "' failed with error message: "                  \
-        << FixMessage(ipu_err_str(__ret.status()))
-#else
-#define APPEND_STATUS_IF_BFRT_ERROR(status, expr)                           \
-  if (const TdiBfStatus __ret = TdiBfStatus(expr)) {                        \
-  } else /* NOLINT */                                                       \
-    status =                                                                \
-        APPEND_ERROR(!status.ok() ? status                                  \
-                                  : ::util::Status(StratumErrorSpace(),     \
-                                                   __ret.error_code(), "")) \
-            .without_logging()                                              \
-        << (status.error_message().empty() ||                               \
-                    status.error_message().back() == ' '                    \
-                ? ""                                                        \
-                : " ")                                                      \
-        << "'" << #expr << "' failed with error message: "                  \
-        << FixMessage(bf_err_str(__ret.status()))
-#endif
+        << FixMessage(tdi_err_str(__ret.status()))
+
 }  // namespace tdi
 }  // namespace hal
 }  // namespace stratum
