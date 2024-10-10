d499eb4ce0879c89b69b5ebde85081cd9ce02745
Derek Foster <derek.foster@intel.com>
2024-10-10 15:26:13 -0700

    Defined P4ExternManager class
    Signed-off-by: Derek Foster <derek.foster@intel.com>

---
+++ stratum/hal/lib/p4/p4_extern_manager.h
@@ -0,0 +1,28 @@
+// Copyright 2023-2024 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+
+#ifndef STRATUM_HAL_LIB_P4_P4_EXTERN_MANAGER_H_
+#define STRATUM_HAL_LIB_P4_P4_EXTERN_MANAGER_H_
+
+#include "p4/config/v1/p4info.pb.h"
+#include "stratum/hal/lib/p4/p4_resource_map.h"
+
+namespace stratum {
+namespace hal {
+
+class P4ExternManager {
+ public:
+  virtual ~P4ExternManager() {}
+
+  virtual void Initialize(const ::p4::config::v1::P4Info& p4info,
+                          PreambleCallback preamble_pb) = 0;
+
+ protected:
+  // Default constructor. To be called by the Mock class instance only.
+  P4ExternManager() {}
+};
+
+}  // namespace hal
+}  // namespace stratum
+
+#endif  // STRATUM_HAL_LIB_P4_P4_EXTERN_MANAGER_H_
