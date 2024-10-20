280fd48617a1368fb0f3dcb1901dfeb565817be2
Derek Foster <derek.foster@intel.com>
2024-10-19 20:02:35 -0700

    Implement 'Get' and 'Set' functions for TdiPktModMeterConfig class
    - All but one of the helper functions in tdi_table_manager gets
      or sets data from a TdiPktModMeterConfig object. It should be
      possible to replace them with Get/Set methods in the class
      itself.
    
    Signed-off-by: Derek Foster <derek.foster@intel.com>

--- stratum/hal/lib/tdi/tdi_pkt_mod_meter_config.h
+++ stratum/hal/lib/tdi/tdi_pkt_mod_meter_config.h
@@ -1,9 +1,10 @@
-// Copyright 2022-2023 Intel Corporation
+// Copyright 2022-2024 Intel Corporation
 // SPDX-License-Identifier: Apache-2.0
 
 #ifndef STRATUM_HAL_LIB_TDI_TDI_PKT_MOD_METER_CONFIG_
 #define STRATUM_HAL_LIB_TDI_TDI_PKT_MOD_METER_CONFIG_
 
+#include "p4/config/v1/p4runtime.pb.h"
 #include "stratum/glue/integral_types.h"
 
 namespace stratum {
@@ -32,6 +33,7 @@ struct TdiPktModMeterConfig {
 
   ~TdiPktModMeterConfig() = default;
 
+  // PolicerMeterConfig
   uint64 meter_prof_id;  // Meter Profile ID
   uint64 cir_unit;       // Committed Information Rate Unit
   uint64 cburst_unit;    // Committed Burst Unit
@@ -41,13 +43,50 @@ struct TdiPktModMeterConfig {
   uint64 cburst;         // Committed Burst Size
   uint64 pir;            // Peak Information Rate
   uint64 pburst;         // Peak Burst Size
+
+  // MeterCounterData
   uint64 greenBytes;     // Green Byte counter
   uint64 greenPackets;   // Green Packet counter
   uint64 yellowBytes;    // Yellow Byte counter
   uint64 yellowPackets;  // Yellow Packet counter
   uint64 redBytes;       // Red Byte counter
   uint64 redPackets;     // Red Packet counter
+
   bool isPktModMeter;    // Flag to check direct or indirect PacketModMeter
+
+  // 'Set' methods retrieve data from the specified object and store it in
+  // the configuration. The parameter is always a const reference.
+
+  void SetPolicerMeterConfig(const ::p4::v1::PolicerMeterConfig& meter_config);
+  void SetMeterCounterData(const ::p4::v1::MeterCounterData& counter_data);
+
+  inline void SetMeterEntry(const ::p4::v1::MeterEntry& meter_entry) {
+    SetPolicerMeterConfig(meter_entry.config().policer_meter_config);
+    SetMeterCounterData(meter_entry.counter_data());
+  }
+
+  inline void SetTableEntry(const ::p4::v1::TableEntry& table_entry) {
+    SetPolicerMeterConfig(table_entry.meter_config().policer_meter_config());
+    SetMeterCounterData(table_entry.meter_counter_data());
+  }
+
+  // 'Get' methods retrieve data from the configuration and store it in
+  // the specified object. The parameter is always a pointer.
+
+  void GetPolicerMeterConfig(::p4::v1::PolicerMeterConfig* meter_config);
+  void GetMeterCounterData(::p4::v1::MeterCounterData* counter_data);
+
+  inline void GetDirectMeterEntry(::p4::v1::DirectMeterEntry* meter_entry) {
+    GetPolicerMeterConfig(
+        meter_entry->mutable_config()->mutable_policer_meter_config());
+    GetMeterCounterData(meter_entry->mutable_counter_data());
+  }
+
+  inline void SetMeterEntry(::p4::v1::MeterEntry* meter_entry) {
+    GetPolicerMeterConfig(
+        meter_entry->mutable_config()->mutable_policer_meter_config());
+    GetMeterCounterData(meter_entry->mutable_counter_data());
+  }
 };
 
 }  // namespace tdi
