fddbf2b890d5b32530e3fd93941626eb90c4886b
Derek Foster <derek.foster@intel.com>
2024-11-06 05:33:26 -0800

    Update port_manager_mock comments
    Signed-off-by: Derek Foster <derek.foster@intel.com>

--- stratum/hal/lib/tdi/dpdk/dpdk_port_manager_mock.cc
+++ stratum/hal/lib/tdi/dpdk/dpdk_port_manager_mock.cc
@@ -3,7 +3,8 @@
 // SPDX-License-Identifier: Apache-2.0
 
 // Dummy implementation of DpdkPortManager for use with
-// DpdkPortManagerMock.
+// DpdkPortManagerMock. Linking this instead of DpdkPortManager
+// when we're using the mock removes the dependency on the SDE.
 
 #include "absl/synchronization/mutex.h"
 #include "stratum/glue/integral_types.h"
