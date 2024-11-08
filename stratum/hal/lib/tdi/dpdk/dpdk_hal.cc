1a1959241c076cc636040396ce25b084d76d07d4
Derek G Foster <derek.foster@intel.com>
2024-11-08 04:06:29 -0800

    Move HAL flag definitions to common file (#310)
    - Extracted the command-line flag definitions used by all three
      TDI HAL implementations and moved them to a common file.
    
    Signed-off-by: Derek Foster <derek.foster@intel.com>

--- stratum/hal/lib/tdi/dpdk/dpdk_hal.cc
+++ stratum/hal/lib/tdi/dpdk/dpdk_hal.cc
@@ -1,6 +1,6 @@
 // Copyright 2018 Google LLC
 // Copyright 2018-present Open Networking Foundation
-// Copyright 2022-2023 Intel Corporation
+// Copyright 2022-2024 Intel Corporation
 // SPDX-License-Identifier: Apache-2.0
 
 #include "stratum/hal/lib/tdi/dpdk/dpdk_hal.h"
@@ -19,34 +19,11 @@
 #include "gflags/gflags.h"
 #include "stratum/glue/logging.h"
 #include "stratum/hal/lib/common/target_options.h"
+#include "stratum/hal/lib/tdi/tdi_hal_flags.h"
 #include "stratum/lib/constants.h"
 #include "stratum/lib/macros.h"
 #include "stratum/lib/security/credentials_manager.h"
 #include "stratum/lib/utils.h"
-
-// TODO(unknown): Use FLAG_DEFINE for all flags.
-DEFINE_string(external_stratum_urls, stratum::kExternalStratumUrls,
-              "Comma-separated list of URLs for server to listen to for "
-              "external calls from SDN controller, etc.");
-DEFINE_string(local_stratum_url, stratum::kLocalStratumUrl,
-              "URL for listening to local calls from stratum stub.");
-
-DEFINE_bool(warmboot, false, "Determines whether HAL is in warmboot stage.");
-DEFINE_string(persistent_config_dir, "/etc/stratum/",
-              "The persistent dir where all the config files will be stored.");
-
-DEFINE_int32(grpc_keepalive_time_ms, 600000, "grpc keep alive time");
-DEFINE_int32(grpc_keepalive_timeout_ms, 20000,
-             "grpc keep alive timeout period");
-DEFINE_int32(grpc_keepalive_min_ping_interval, 10000,
-             "grpc keep alive minimum ping interval");
-DEFINE_int32(grpc_keepalive_permit, 1, "grpc keep alive permit");
-DEFINE_uint32(grpc_max_recv_msg_size, 256 * 1024 * 1024,
-              "grpc server max receive message size (0 = gRPC default).");
-DEFINE_uint32(grpc_max_send_msg_size, 0,
-              "grpc server max send message size (0 = gRPC default).");
-DEFINE_bool(grpc_open_insecure_mode, false,
-            "open grpc server ports in insecure mode for gNMI, gNOI, and P4RT");
 
 DECLARE_string(forwarding_pipeline_configs_file);
 
