a43d4fcdfe29f21f8714179b557edf1485cae8fe
Derek G Foster <derek.foster@intel.com>
2024-11-12 16:11:20 -0800

    Fix RejectMeterEntryInsertDelete test failure (#319)
    The TdiTableManagerTest::RejectMeterEntryInsertDelete test case
    was failing because WriteMeterEntry() was returning UNKNOWN (2)
    instead of the expected ERR_INVALID_PARAM. The failure was in
    the second half of the test case (the DELETE check).
    
    The INSERT and DELETE checks are unrelated. The former involves
    validating the `type` parameter; the latter involves validating
    the meter entry. The two do not belong in the same test case.
    
    The objective is to fix a broken test case. Developing new ones
    is out of scope.  Removed the DELETE check, renamed the test
    case to RejectMeterEntryInsert, and enabled it.
    
    Signed-off-by: Derek Foster <derek.foster@intel.com>

--- stratum/hal/lib/tdi/tdi_table_manager_test.cc
+++ stratum/hal/lib/tdi/tdi_table_manager_test.cc
@@ -324,8 +324,7 @@ TEST_F(TdiTableManagerTest, RejectMeterEntryModifyWithoutMeterId) {
   EXPECT_THAT(ret.error_message(), HasSubstr("Missing meter id"));
 }
 
-// See https://github.com/ipdk-io/stratum-dev/issues/314.
-TEST_F(TdiTableManagerTest, DISABLED_RejectMeterEntryInsertDelete) {
+TEST_F(TdiTableManagerTest, RejectMeterEntryInsert) {
   ASSERT_OK(PushTestConfig());
   auto session_mock = std::make_shared<SessionMock>();
 
@@ -347,13 +346,6 @@ TEST_F(TdiTableManagerTest, DISABLED_RejectMeterEntryInsertDelete) {
   ::util::Status ret = tdi_table_manager_->WriteMeterEntry(
       session_mock, ::p4::v1::Update::INSERT, entry);
   ASSERT_FALSE(ret.ok());
-  EXPECT_EQ(ERR_INVALID_PARAM, ret.error_code());
-
-  ret = tdi_table_manager_->WriteMeterEntry(session_mock,
-                                            ::p4::v1::Update::DELETE, entry);
-  ASSERT_FALSE(ret.ok());
-  // TODO(derek): fails because WriteMeterEntry returns UNKNOWN (2)
-  // instead of ERR_INVALID_PARAM (503).
   EXPECT_EQ(ERR_INVALID_PARAM, ret.error_code());
 }
 
