2f154bc43f4295602a20807c80d7c753d4802fcb
Sabeel Ansari <sabeel.ansari@intel.com>
2025-01-30 12:44:28 -0800

    Update OperStatus and MacAddress retrievals to use get-with-val functor
    Signed-off-by: Sabeel Ansari <sabeel.ansari@intel.com>

--- stratum/hal/lib/yang/yang_parse_tree_virtual_port.cc
+++ stratum/hal/lib/yang/yang_parse_tree_virtual_port.cc
@@ -24,37 +24,6 @@ namespace {
 ////////////////////////////////////////////////////////////////////////////////
 // /virtual-ports/virtual-port/state/vsi
 void SetUpVirtualPortFetchVSI(TreeNode* node, YangParseTree* tree) {
-#if 0
-  auto poll_functor = [tree](const GnmiEvent& event, const ::gnmi::Path& path,
-                             GnmiSubscribeStream* stream) {
-    // Create a data retrieval request.
-    DataRequest req;
-    auto* request = req.add_requests()->mutable_vport_vsi();
-
-    ::gnmi::Path* path_ptr = const_cast<::gnmi::Path*>(&path);
-    
-    std::cout << ">> SABEEL2 path=" << path_ptr->DebugString() << std::endl;
-
-    request->set_global_resource_id(0);  // TODO sabeel
-
-    // In-place definition of method retrieving data from generic response
-    // and saving into 'resp' local variable.
-    uint32 resp{};
-    DataResponseWriter writer([&resp](const DataResponse& in) {
-      if (!in.has_vport_vsi()) return false;
-      resp = in.vport_vsi().vsi();
-      return true;
-    });
-    // Query the switch. The returned status is ignored as there is no way to
-    // notify the controller that something went wrong. The error is logged when
-    // it is created.
-    tree->GetSwitchInterface()
-        ->RetrieveValue(/*node_id*/ 0, req, &writer, /* details= */ nullptr)
-        .IgnoreError();
-    return SendResponse(GetResponse(path, resp), stream);
-  };
-#endif
-
   auto get_functor = [tree](const ::gnmi::Path& path,
                             const std::vector<std::string>& val,
                             GnmiSubscribeStream* stream) {
@@ -95,11 +64,16 @@ void SetUpVirtualPortFetchVSI(TreeNode* node, YangParseTree* tree) {
 ////////////////////////////////////////////////////////////////////////////////
 // /virtual-ports/virtual-port/state/oper-status
 void SetUpVirtualPortFetchOperStatus(TreeNode* node, YangParseTree* tree) {
-  auto poll_functor = [tree](const GnmiEvent& event, const ::gnmi::Path& path,
-                             GnmiSubscribeStream* stream) {
+  auto get_functor = [tree](const ::gnmi::Path& path,
+                            const std::vector<std::string>& val,
+                            GnmiSubscribeStream* stream) {
     DataRequest req;
-    auto* request = req.add_requests()->mutable_vport_oper_status();
-    request->set_global_resource_id(0);  // TODO sabeel
+    auto* request = req.add_requests()->mutable_vport_vsi();
+    ::gnmi::Path* path_ptr = const_cast<::gnmi::Path*>(&path);
+
+    uint32 glort_id = static_cast<uint32>(
+        std::stoul(val.at(0)));  // only one will be present at index 0
+    request->set_global_resource_id(glort_id);
 
     uint32 resp{};
     DataResponseWriter writer([&resp](const DataResponse& in) {
@@ -112,20 +86,27 @@ void SetUpVirtualPortFetchOperStatus(TreeNode* node, YangParseTree* tree) {
         .IgnoreError();
     return SendResponse(GetResponse(path, resp), stream);
   };
-  auto on_change_functor = UnsupportedFunc();
-  node->SetOnPollHandler(poll_functor)
-      ->SetOnTimerHandler(poll_functor)
-      ->SetOnChangeHandler(on_change_functor);
+
+  auto unsupported_functor = UnsupportedFunc();
+  node->SetOnPollHandler(unsupported_functor)
+      ->SetOnTimerHandler(unsupported_functor)
+      ->SetOnGetWithValHandler(get_functor)
+      ->SetOnChangeHandler(unsupported_functor);
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 // /virtual-ports/virtual-port/state/mac-address
 void SetUpVirtualPortFetchMacAddress(TreeNode* node, YangParseTree* tree) {
-  auto poll_functor = [tree](const GnmiEvent& event, const ::gnmi::Path& path,
-                             GnmiSubscribeStream* stream) {
+  auto get_functor = [tree](const ::gnmi::Path& path,
+                            const std::vector<std::string>& val,
+                            GnmiSubscribeStream* stream) {
     DataRequest req;
-    auto* request = req.add_requests()->mutable_vport_mac_address();
-    request->set_global_resource_id(0);  // TODO sabeel
+    auto* request = req.add_requests()->mutable_vport_vsi();
+    ::gnmi::Path* path_ptr = const_cast<::gnmi::Path*>(&path);
+
+    uint32 glort_id = static_cast<uint32>(
+        std::stoul(val.at(0)));  // only one will be present at index 0
+    request->set_global_resource_id(glort_id);
 
     uint32 resp{};
     DataResponseWriter writer([&resp](const DataResponse& in) {
@@ -139,10 +120,12 @@ void SetUpVirtualPortFetchMacAddress(TreeNode* node, YangParseTree* tree) {
     return SendResponse(GetResponse(path, MacAddressToYangString(resp)),
                         stream);
   };
-  auto on_change_functor = UnsupportedFunc();
-  node->SetOnPollHandler(poll_functor)
-      ->SetOnTimerHandler(poll_functor)
-      ->SetOnChangeHandler(on_change_functor);
+
+  auto unsupported_functor = UnsupportedFunc();
+  node->SetOnPollHandler(unsupported_functor)
+      ->SetOnTimerHandler(unsupported_functor)
+      ->SetOnGetWithValHandler(get_functor)
+      ->SetOnChangeHandler(unsupported_functor);
 }
 
 }  // namespace
